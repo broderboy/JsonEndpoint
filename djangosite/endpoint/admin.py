6bc10379c821699a3559f034cc1713a36d431514
Tim Broder <timothy.broder@gmail.com>
2014-02-21 14:52:59 -0500

    Made the text area bigger

--- djangosite/endpoint/admin.py
+++ djangosite/endpoint/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
+from django.forms import Textarea
+from django.db import models
+
 from models import JsonEndpoint
 import reversion
 
 class JsonEndpointAdmin(reversion.VersionAdmin):
 	model = JsonEndpoint
 	readonly_fields = ('slug',)
+	formfield_overrides = {
+        models.TextField: {'widget': Textarea(attrs={'rows':40, 'cols':200})},
+    }
+
 
 admin.site.register(JsonEndpoint, JsonEndpointAdmin)
